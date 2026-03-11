6087249c4d987983e46b95b63035d190df83b77b
Kenneth Sarmaat <knnth@outlook.com>
2026-03-11 15:29:15 +0100

    First version of simulator

---
+++ ConsoleTester/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MiniSimulator.Domain;
+
+var sim = new Simulator();
+
+var homeTeam = new Team("FC Home", 0.8f, .4f, 1, false);
+var awayTeam = new Team("Visitors United", .6f, .0f, .8f, false);
+
+for (int i = 0; i < 10; i++)
+{
+    var group = new Group([new Match(homeTeam, awayTeam)]);
+
+    sim.Simulate(group);
+
+    Console.WriteLine(group.Matches.First());
+}
--- MiniSimulator.Domain/Match.cs
+++ MiniSimulator.Domain/Match.cs
@@ -7,4 +7,7 @@ public class Match(Team homeTeam, Team awayTeam)
     public bool IsPlayed { get; set; } = false;
     public int HomeGoals { get; set; }
     public int AwayGoals { get; set; }
+
+    public override string ToString() =>
+        $"{HomeTeam.Name} - {AwayTeam.Name}" + (IsPlayed ? $": {HomeGoals} -  {AwayGoals}" : "");
 }
